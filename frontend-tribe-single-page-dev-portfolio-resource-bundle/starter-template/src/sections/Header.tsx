a70f3753559aea4fd97a739d58a6ff5a3cab97c6
Feddane <chafeddane@gmail.com>
2025-06-14 21:48:54 +0100

    config and fonts

--- frontend-tribe-single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Header.tsx
+++ frontend-tribe-single-page-dev-portfolio-resource-bundle/starter-template/src/sections/Header.tsx
@@ -1,3 +1,13 @@
+
+
+
 export const Header = () => {
-  return <div>Header Section</div>;
+  return <div>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Projects</a> 
+      <a href="#">About</a> 
+      <a href="#">Contact</a> 
+    </nav>
+  </div>;
 };
